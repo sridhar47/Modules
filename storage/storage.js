d2c3481d0e3a364f7aa6ffe4df01469bd793aca8
Eduardo Ottaviani <Edu@ottaviani.local>
2015-09-11 12:04:42 -0300

    Removing functions support, let json be json

--- storage/storage.js
+++ storage/storage.js
@@ -1,32 +1,13 @@
 define(function(){
-
-	//https://gist.github.com/cowboy/3749767
-	function stringify(obj, prop){
-		var placeholder = '____PLACEHOLDER____';
-		var fns = [];
-		var json = JSON.stringify(obj, function(key, value) {
-
-				if (typeof value === 'function') {
-					fns.push(value);
-					return placeholder;
-				}
-				return value;
-			}, 2);
-
-			json = json.replace(new RegExp('"' + placeholder + '"', 'g'), function(_) {
-				return fns.shift();
-			});
-			return json;
-	}
-
+	
 	return{
 		set :function( name, data ){
-			localStorage.setItem( name, stringify( data ) );
+			localStorage.setItem( name, JSON.stringify( data ) );
 			return data;
 		},
 
 		get :function( name ){
-			return (new Function( 'return ' + localStorage.getItem( name ) ) )();
+			return JSON.parse(localStorage.getItem( name ));
 		},
 
 		remove :function( name ){
