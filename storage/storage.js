0fac2d157f4aafe19817cf69c4f16cc849b3d25b
Eduardo Ottaviani <Edu@ottaviani.local>
2015-09-11 11:56:48 -0300

    Adding functions support

--- storage/storage.js
+++ storage/storage.js
@@ -1,13 +1,32 @@
 define(function(){
-	
+
+	//https://gist.github.com/cowboy/3749767
+	function stringify(obj, prop){
+		var placeholder = '____PLACEHOLDER____';
+		var fns = [];
+		var json = JSON.stringify(obj, function(key, value) {
+
+				if (typeof value === 'function') {
+					fns.push(value);
+					return placeholder;
+				}
+				return value;
+			}, 2);
+
+			json = json.replace(new RegExp('"' + placeholder + '"', 'g'), function(_) {
+				return fns.shift();
+			});
+			return 'this["' + prop + '"] = ' + json + ';';
+	}
+
 	return{
 		set :function( name, data ){
-			localStorage.setItem( name, JSON.stringify( data ) );
+			localStorage.setItem( name, stringify( data ) );
 			return data;
 		},
 
 		get :function( name ){
-			return JSON.parse(localStorage.getItem( name ));
+			return (new Function( 'return ' + localStorage.getItem( name ) ) )();
 		},
 
 		remove :function( name ){
